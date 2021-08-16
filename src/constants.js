653d16d0030641f7752e4721f637323ce45c96b8
andresberrios <andres.berrios.j@gmail.com>
2021-08-16 10:38:27 +0200

    Export a Tailwind class to use programmatically

--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const VIRTUAL_SOURCE_PATH = '/sourcePath';
-export const VIRTUAL_HTML_FILENAME = '/htmlInput';
--- src/main.js
+++ src/main.js
@@ -1,18 +1,54 @@
-import observerScript from './observer';
+// Maybe we need this for having colors, but not sure:
+// eslint-disable-next-line no-unused-vars
+import colors from 'tailwindcss/colors';
 
-(() => {
-  const config = {
-    attributes: true,
-    attributeFilter: ['class'],
-    childList: true,
-    subtree: true
-  };
+import postcss from 'postcss';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-  new MutationObserver(observerScript(false)).observe(document.documentElement, config);
+const VIRTUAL_HTML_FILENAME = '/tailwindVirtualHtmlInput';
 
-  observerScript();
+const defaultConfig = {
+  mode: 'jit',
+  purge: [],
+  safelist: [],
+  theme: {},
+  plugins: []
+};
 
-  window.tailwindCSS = {
-    refresh: observerScript(true)
-  };
-})();
+export default class Tailwind {
+  constructor(config = {}, customCss = '') {
+    this.config = { ...defaultConfig, ...config };
+    this.customCss = customCss;
+  }
+
+  async process(htmlContent = '', classes = []) {
+    self[VIRTUAL_HTML_FILENAME] = htmlContent;
+
+    const result = await postcss([
+      tailwindcss({
+        ...this.config,
+        ...{ purge: [VIRTUAL_HTML_FILENAME], safelist: classes }
+      }),
+      autoprefixer
+    ]).process(
+      `
+      @tailwind base;
+      @tailwind components;
+      ${this.customCss}
+      @tailwind utilities;
+      `,
+      { from: '/virtualSourcePath', map: false }
+    );
+
+    return result.css;
+  }
+
+  async processClasses(classes) {
+    return this.process('', classes);
+  }
+
+  async processHtml(htmlContent) {
+    return this.process(htmlContent, []);
+  }
+}
